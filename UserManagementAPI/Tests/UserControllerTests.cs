fde4aa1b7aad6b1286aef1081db82933a586cfd2
77menzs <77menzs@gmail.com>
2025-03-25 09:16:38 +0100

    add basic unit test

---
+++ UserManagementAPI/Tests/UserControllerTests.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Controllers;
+using UserManagementAPI.Models;
+using Xunit;
+
+public class UserControllerTests
+{
+    [Fact]
+    public void GetUsers_ReturnsAllUsers()
+    {
+        // Arrange
+        var controller = new UserController();
+
+        // Act
+        var result = controller.GetUsers();
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var users = Assert.IsType<List<User>>(okResult.Value);
+        Assert.Equal(5, users.Count); // Assuming there are 5 users in the static list
+    }
+
+    [Fact]
+    public void GetUser_ReturnsUser_WhenIdIsValid()
+    {
+        // Arrange
+        var controller = new UserController();
+        int validId = 1;
+
+        // Act
+        var result = controller.GetUser(validId);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var user = Assert.IsType<User>(okResult.Value);
+        Assert.Equal(validId, user.Id);
+    }
+
+    [Fact]
+    public void GetUser_ReturnsNotFound_WhenIdIsInvalid()
+    {
+        // Arrange
+        var controller = new UserController();
+        int invalidId = 999;
+
+        // Act
+        var result = controller.GetUser(invalidId);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+    }
+
+    [Fact]
+    public void CreateUser_AddsUserAndReturnsCreatedAtAction()
+    {
+        // Arrange
+        var controller = new UserController();
+        var newUser = new User { FirstName = "Test", LastName = "User", Email = "test.user@example.com", Role = "Tester" };
+
+        // Act
+        var result = controller.CreateUser(newUser);
+
+        // Assert
+        var createdAtActionResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+        var createdUser = Assert.IsType<User>(createdAtActionResult.Value);
+        Assert.Equal(6, createdUser.Id); // Assuming the new user gets ID 6
+    }
+
+    [Fact]
+    public void DeleteUser_RemovesUser_WhenIdIsValid()
+    {
+        // Arrange
+        var controller = new UserController();
+        int validId = 1;
+
+        // Act
+        var result = controller.DeleteUser(validId);
+
+        // Assert
+        Assert.IsType<NoContentResult>(result);
+        Assert.DoesNotContain(controller.GetUsers().Value, u => u.Id == validId);
+    }
+}
