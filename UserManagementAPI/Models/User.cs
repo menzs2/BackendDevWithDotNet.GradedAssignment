061bfbbcad82d0db17569738049e31a81cc01c3a
77menzs <77menzs@gmail.com>
2025-03-25 08:12:38 +0100

    Add user model

---
+++ UserManagementAPI/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagementAPI;
+
+public class User
+{
+    public int Id { get; set; }
+    required public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? Email { get; set; }
+    public string? Role { get; set; }
+}
